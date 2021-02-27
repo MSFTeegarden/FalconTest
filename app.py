e55db245f71598f3e7fe1f1e8bbaffc22529b805
Kyle Teegarden <kyle.teegarden@microsoft.com>
2021-02-26 16:23:38 -0800

    small updates

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import falcon
 import sys
 import time
 import pyodbc
+import redis
+from datetime import datetime
 
 # connection parameters--replace with your own info
 server = 'benchmarkingserver.database.windows.net'
@@ -11,6 +13,7 @@ username = 'kyle'
 password = 'password1234!'
 driver = '{ODBC Driver 17 for SQL Server}'
 
+# connect to the database
 conn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password, autocommit=True)
 cur = conn.cursor()
 cur.execute('SELECT @@version')
@@ -18,72 +21,63 @@ db_version = cur.fetchone()[0]
 print(db_version, flush=True)
 print('logged in', flush=True)  # Note: adding 'flush=True' after the print statement causes falcon to print the line to console.
 
-def create_destroy_session(id, action): # This method handles the "login" and "logout" API calls
-    if (action == '/login'):
-        cur.execute("select sessionId from sessions where sessionID = %d" %int(id)) # see if session exists in database
-        r = cur.fetchall()
-        if (r):
-            print(str(r), flush=True)
-            print('session already exists', flush=True)
-            cur.execute("update sessions set isActive = '1' where sessionID = %d" %int(id)) # update active status
-        else:
-            cur.execute("insert into sessions values (%d, '1', GETDATE(), NULL)" %int(id)) # if session doesn't exist, create new one
-            print('new session created', flush=True)
+# connect to the Redis instance
+r = redis.Redis(host='redisbenchmark.redis.cache.windows.net', port=6380, db=0, password='qKP1nKP58PtVkGHhqdIE0a0hbcrWELrVV4SkcWUae8o=', ssl=True, charset="utf-8", decode_responses=True)
+print('Redis v.' + r.execute_command('INFO')['redis_version'], flush=True)
 
+
+def create_destroy_session(id, action): # This method handles the "login" and "logout" API calls
+    store_dt = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S.%f')
+    if (action == '/login'):
+        r.hmset("session:" + id, { "isActive": 1, "activeDate": store_dt })
+    
     elif (action == '/logout'):
-        cur.execute("update sessions set isActive = '0', inactiveDate = GETDATE() where sessionID = %d" % int(id)) # set session to inactive
-        print('logout complete', flush=True)
-    token = id
+        r.hmset("session:" + id, { "isActive": 0, "inactiveDate": store_dt })
+    
+    token = r.hgetall("session:" + id)
     return token # return the session ID
 
 def add_remove_view_item(id, item, action): # this method handles the "view", "add", and "remove" API calls
+    global r
     print('starting process', flush=True)
-    cur.execute("select sessionId from sessions where sessionID = %d and isActive= '1'" % int(id)) # check if session exists and is active
-    r = cur.fetchall()
-    print('session check complete', flush=True)
     print(str(r), flush=True)
-    
-    if (r): #If the session is active
-        
-        if (action == '/view'):
 
+    if (r.hget("session:" + id, "isActive") == '1'): # check if session exists and is active
+        print('session check complete', flush=True)
+
+        if (action == '/view'):
             print('action: view', flush=True)
-            cur.execute("select price from items where itemID = %d" % int(item)) #select item price from given itemID
-            r = cur.fetchall()
+            price = r.get("item:" + item)  # check cache for price
+            if not price:
+                print('no price found in cache', flush=True)
+                cur.execute("select price from items where itemID = %d" % int(item))  # if price is not in cache, check DB
+                price = cur.fetchall()[0][0]
+                r.set("item:" + item, str(price))  # add price to cache
+            
             print('sucessful price check', flush=True)
-            print(str(r[0][0]), flush=True)
-            return str(r[0][0]) # return the price
+            print(str(price), flush=True)
+            return str(price) # return the price
         
         elif (action == '/add'):
+            store_dt = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S.%f')
+            price = r.get("item:" + item)
+            r.hmset("cart:" + id + ":item:" + item, { "price": price,"addedDate": store_dt })
+            return True
 
-            cur.execute("select price from items where itemID = %d" % int(item)) #select item price from given itemID
-            r = cur.fetchall()
-            if(r): # if the item exists
-                cur.execute("select price from carts where sessionId = %d and itemId = %d" %(int(id), int(item))) # lookup item to see if is already in cart
-                r = cur.fetchall()
-                if(r):
-                    cur.execute("insert into carts values (%d, %d, %.2f, GETDATE())" % (int(id),int(item), float(r[0][0]))) # add item to cart
-                    return True
-                else:
-                    return False
-            else:
-                return False
-        
         elif (action == '/remove'):
-
-            cur.execute("select price from carts where sessionId = %d and itemId = %d" %(int(id), int(item))) # lookup item to see if it exists
-            r = cur.fetchall()
-            if(r): # if it exists
-                cur.execute("delete from carts where sessionId = %d and itemId = %d" %(int(id), int(item))) # delete item from cart
+            if (r.exists("cart:" + id + ":item:" + item)):
+                r.delete("cart:" + id + ":item:" + item)
                 return True
             else:
+                print('item not in cart', flush=True)
                 return False
         
         else:
             return False
     else:
+        print('session not found', flush=True)
         return False
-    return False
+    return True
 
 
 class SessionResource(object):
